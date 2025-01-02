6cb8bd6769a175f4350e5e570458e3eb8c6fe058
shrey017 <137369467+KnightMare-shrey@users.noreply.github.com>
2025-01-03 01:37:46 +0530

    Update removing_digits.cpp

--- removing_digits.cpp
+++ removing_digits.cpp
@@ -1,34 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long
-int dp[1000100];
-set<int> getdigits(int x){
-	set<int> s;
-	while(x){
-		s.insert(x%10);
-		x=x/10;
-	}
-	
-	return s;
-}
 
-void solve(int n){
-	int ans=INT_MAX;
-	for(int i=0;i<n;i++){
-		if(i==0) dp[i]=0;
-		else{
-			dp[i]=INT_MAX;
-			for(auto it: getdigits(i)){
-				if(i>=it) dp[i]=min(dp[i],1+dp[i-it]);
-			}
-		}
-	}
-	
-	cout<<dp[n]<<endl; 
-}
-int main(){
-	ios_base::syn_with_stdio(0);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;cin>>n;
+int main() {
+  int n;
+  cin >> n;
+  vector<int> dp(n+1,1e9);
+  dp[0] = 0;
+  for (int i = 0; i <= n; i++) {
+    for (char c : to_string(i)) {
+      dp[i] = min(dp[i], dp[i-(c-'0')]+1);
+    }
+  }
+  cout << dp[n] << endl;
 }
